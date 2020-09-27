332c0295eb2f1643149e952ddee1d9aa4a4dd8a9
psgatwork <psgatwork@gmail.com>
2020-09-27 14:10:35 +0530

    Initial Commit to the solution.

---
+++ src/webparts/justOne/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IJustOneWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'JustOneWebPartStrings' {
+  const strings: IJustOneWebPartStrings;
+  export = strings;
+}
